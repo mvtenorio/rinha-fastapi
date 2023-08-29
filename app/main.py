4f8677b2535cc3da8448fe9fe191b9a24d8ae972
Marcus Tenório <marcusvtenorio@gmail.com>
2023-08-29 00:01:34 -0300

    Use db

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from datetime import date
 from uuid import UUID, uuid4
 
-from fastapi import FastAPI, HTTPException, Response, status
+from fastapi import Depends, FastAPI, HTTPException, Response, status
+from psycopg.rows import dict_row
+from psycopg_pool import AsyncConnectionPool
 from pydantic import BaseModel
 
 app = FastAPI()
+
+
+async def get_db():
+    pool = AsyncConnectionPool(conninfo="postgresql://postgres:postgres@db:5432/rinha")
+    async with pool.connection() as conn:
+        yield conn
 
 
 class PessoaIn(BaseModel):
@@ -18,9 +26,44 @@ class PessoaOut(PessoaIn):
     id: UUID
 
 
-@app.post("/pessoas", response_model=PessoaOut, status_code=status.HTTP_201_CREATED)
-def create_pessoa(pessoa_in: PessoaIn, response: Response):
+class PessoaNotFound(Exception):
+    ...
+
+
+async def insert_pessoa(pessoa_in, db):
     pessoa_out = PessoaOut(id=uuid4(), **pessoa_in.dict())
+
+    await db.execute(
+        "INSERT INTO pessoas (id, apelido, nome, nascimento, stack) "
+        "VALUES (%(id)s, %(apelido)s, %(nome)s, %(nascimento)s, %(stack)s)",
+        pessoa_out.model_dump(),
+    )
+    return pessoa_out
+
+
+async def select_pessoa(pessoa_id, db):
+    cursor = await db.cursor(row_factory=dict_row).execute(
+        "SELECT id, apelido, nome, nascimento, stack FROM pessoas p WHERE p.id = %(id)s LIMIT 1",
+        {"id": str(pessoa_id)},
+    )
+    pessoa = await cursor.fetchone()
+
+    if not pessoa:
+        raise PessoaNotFound()
+
+    return PessoaOut(
+        id=pessoa_id,
+        apelido=pessoa["apelido"],
+        nome=pessoa["nome"],
+        nascimento=pessoa["nascimento"],
+        stack=pessoa["stack"][1:-1].split(",") if pessoa["stack"] else None,
+    )
+
+
+@app.post("/pessoas", response_model=PessoaOut, status_code=status.HTTP_201_CREATED)
+async def create_pessoa(pessoa_in: PessoaIn, response: Response, db=Depends(get_db)):
+    pessoa_out = await insert_pessoa(pessoa_in, db)
+
     response.headers["Location"] = app.url_path_for(
         "show_pessoa", pessoa_id=pessoa_out.id
     )
@@ -29,14 +72,13 @@ def create_pessoa(pessoa_in: PessoaIn, response: Response):
 
 
 @app.get("/pessoas/{pessoa_id}", response_model=PessoaOut)
-def show_pessoa(pessoa_id: UUID):
-    return PessoaOut(
-        id=pessoa_id,
-        nome="Nome",
-        apelido="Apelido",
-        nascimento="2023-08-28",
-        stack=["Python"],
-    )
+async def show_pessoa(pessoa_id: UUID, db=Depends(get_db)):
+    try:
+        return await select_pessoa(pessoa_id, db)
+    except PessoaNotFound:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Pessoa não encontrada"
+        )
 
 
 @app.get("/pessoas", response_model=list[PessoaOut])
