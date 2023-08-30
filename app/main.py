be6e8ae716eb25a2d83d43d2e81ebe8c33bb7a0c
Marcus Tenório <marcusvtenorio@gmail.com>
2023-08-30 01:16:32 -0300

    Use redis

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ from datetime import date
 from uuid import UUID, uuid4
 
 from fastapi import Depends, FastAPI, HTTPException, Response, status
+from psycopg.errors import UniqueViolation
 from psycopg.rows import dict_row
 from psycopg_pool import AsyncConnectionPool
 from pydantic import BaseModel, Field, constr
+from redis import Redis
 
 app = FastAPI()
 
@@ -13,6 +15,10 @@ async def get_db():
     pool = AsyncConnectionPool(conninfo="postgresql://postgres:postgres@db:5432/rinha")
     async with pool.connection() as conn:
         yield conn
+
+
+async def get_redis():
+    yield Redis(host="redis", port=6379, decode_responses=True)
 
 
 class PessoaIn(BaseModel):
@@ -37,14 +43,27 @@ class PessoaOut(PessoaIn):
 
 
 @app.post("/pessoas", response_model=PessoaOut, status_code=status.HTTP_201_CREATED)
-async def create_pessoa(pessoa_in: PessoaIn, response: Response, db=Depends(get_db)):
+async def create_pessoa(
+    pessoa_in: PessoaIn,
+    response: Response,
+    db=Depends(get_db),
+    redis=Depends(get_redis),
+):
     pessoa_out = PessoaOut(**pessoa_in.dict())
 
-    await db.execute(
-        "INSERT INTO pessoas (id, apelido, nome, nascimento, stack) "
-        "VALUES (%(id)s, %(apelido)s, %(nome)s, %(nascimento)s, %(stack)s)",
-        pessoa_out.model_dump(),
-    )
+    try:
+        await db.execute(
+            "INSERT INTO pessoas (id, apelido, nome, nascimento, stack) "
+            "VALUES (%(id)s, %(apelido)s, %(nome)s, %(nascimento)s, %(stack)s)",
+            pessoa_out.model_dump(),
+        )
+    except UniqueViolation:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Já existe uma pessoa com esse apelido",
+        )
+
+    redis.set(f"pessoas:{pessoa_out.id}", pessoa_out.model_dump_json())
 
     response.headers["Location"] = app.url_path_for(
         "show_pessoa", pessoa_id=pessoa_out.id
@@ -53,9 +72,12 @@ async def create_pessoa(pessoa_in: PessoaIn, response: Response, db=Depends(get_
     return pessoa_out
 
 
-
 @app.get("/pessoas/{pessoa_id}", response_model=PessoaOut)
-async def show_pessoa(pessoa_id: UUID, db=Depends(get_db)):
+async def show_pessoa(pessoa_id: UUID, db=Depends(get_db), redis=Depends(get_redis)):
+    pessoa_from_cache = redis.get(f"pessoas:{pessoa_id}")
+    if pessoa_from_cache:
+        return PessoaOut.model_validate_json(pessoa_from_cache)
+
     cursor = await db.cursor(row_factory=dict_row).execute(
         "SELECT id, apelido, nome, nascimento, stack FROM pessoas p WHERE p.id = %(id)s LIMIT 1",
         {"id": str(pessoa_id)},
@@ -67,7 +89,10 @@ async def show_pessoa(pessoa_id: UUID, db=Depends(get_db)):
             status_code=status.HTTP_404_NOT_FOUND, detail="Pessoa não encontrada"
         )
 
-    return PessoaOut.from_dict(pessoa)
+    pessoa_out = PessoaOut.from_dict(pessoa)
+    redis.set(f"pessoas:{pessoa_out.id}", pessoa_out.model_dump_json())
+
+    return pessoa_out
 
 
 @app.get("/pessoas", response_model=list[PessoaOut])
@@ -86,4 +111,3 @@ async def contagem_pessoas(db=Depends(get_db)):
     cursor = await db.execute("SELECT COUNT(*) FROM pessoas")
     result = await cursor.fetchone()
     return result[0]
-
